f991bbffea31b4310dee417b4a06fc6b67d8c483
Ariel Borensztein <ariboren@gmail.com>
2022-01-04 13:36:31 -0500

    Update types

--- types/NewWindow.d.ts
+++ types/NewWindow.d.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-declare module 'react-new-window' {
+declare module '@playback-sports/react-new-window' {
   type EventHandler = () => void
   type OpenEventHandler = (window: Window) => void
 
